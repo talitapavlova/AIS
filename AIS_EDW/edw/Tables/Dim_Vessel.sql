6425a79820329980b9af2835631d750186d12a96
Stefy8700isstefy8700isc <stefy8700isc@gmail.com>
2020-03-05 10:21:59 +0100

    Add MMSI, IMO col to Dim_Vessel

--- AIS_EDW/edw/Tables/Dim_Vessel.sql
+++ AIS_EDW/edw/Tables/Dim_Vessel.sql
@@ -1,9 +1,13 @@
 ﻿CREATE TABLE [edw].[Dim_Vessel] (
     [Vessel_Key]  INT            IDENTITY (1, 1) NOT NULL,
     [ID]          NVARCHAR (256) NULL,
+    [MMSI]        INT            NULL,
+    [IMO]         INT            NULL,
     [ValidFrom]   DATE           NULL,
     [ValidTo]     DATE           NULL,
     [DateCreated] DATETIME2 (7)  CONSTRAINT [DF_Vessel] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_Vessel] PRIMARY KEY CLUSTERED ([Vessel_Key] ASC)
 );
+
+
 

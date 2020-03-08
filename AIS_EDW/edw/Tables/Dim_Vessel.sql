d4071679c33b9b86b08e1ce27a6f47e3f96cdcde
Stefy8700isstefy8700isc <stefy8700isc@gmail.com>
2020-03-08 23:10:37 +0100

    Extract_JSON, Little bits of VIEW

--- AIS_EDW/edw/Tables/Dim_Vessel.sql
+++ AIS_EDW/edw/Tables/Dim_Vessel.sql
@@ -3,10 +3,10 @@
     [ID]             NVARCHAR (256) NULL,
     [MMSI]           CHAR (9)       NULL,
     [IMO]            CHAR (10)      NULL,
-    [Type of mobile] NVARCHAR (20)  NULL,
-    [Call Sign]      NVARCHAR (20)  NULL,
+    [Type_of_mobile] NVARCHAR (20)  NULL,
+    [Call_Sign]      NVARCHAR (20)  NULL,
     [Name]           NVARCHAR (50)  NULL,
-    [Ship type]      NVARCHAR (50)  NULL,
+    [Ship_type]      NVARCHAR (50)  NULL,
     [Width]          DECIMAL (18)   NULL,
     [Length]         DECIMAL (18)   NULL,
     [Draught]        DECIMAL (18)   NULL,
@@ -15,6 +15,8 @@
     [DateCreated]    DATETIME2 (7)  CONSTRAINT [DF_Vessel] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_Vessel] PRIMARY KEY CLUSTERED ([Vessel_Key] ASC)
 );
+
+
 
 
 
--- AIS_Stage/stage/Views/VIEW_DimVessel.sql
+++ AIS_Stage/stage/Views/VIEW_DimVessel.sql
@@ -1,5 +1,18 @@
 ﻿
+
 CREATE VIEW [stage].[VIEW_DimVessel]
 AS
-SELECT        Type_of_mobile, MMSI, IMO, Callsign, Name, Ship_type, Cargo_type, Width, Length, Draught
-FROM            dbo.Extract_Vessel
+	WITH temp_VesselInfo_1 as
+	(Select 
+			Type_of_mobile, 
+			MMSI, 
+			IMO, 
+			Callsign, 
+			Name, 
+			Ship_type, 
+			Cargo_type, 
+			Width, 
+			Length, 
+			Draught 
+		FROM dbo.Extract_Vessel)
+	SELECT * FROM temp_VesselInfo_1

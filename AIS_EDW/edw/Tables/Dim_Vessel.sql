115cdc8e4042ae08ddf5b98cc63ccddeaafbea98
stefy8700isc <stefy8700isc@gmail.com>
2020-03-27 01:43:27 +0100

    extra_checks_vessel

--- AIS_EDW/edw/Tables/Dim_Vessel.sql
+++ AIS_EDW/edw/Tables/Dim_Vessel.sql
@@ -1,19 +1,21 @@
 ﻿CREATE TABLE [edw].[Dim_Vessel] (
-    [Vessel_Key]     INT           IDENTITY (1, 1) NOT NULL,
-    [MMSI]           CHAR (9)      NULL,
-    [IMO]            CHAR (10)     NULL,
-    [Type_of_mobile] NVARCHAR (20) NULL,
-    [Call_Sign]      NVARCHAR (20) NULL,
-    [Name]           NVARCHAR (50) NULL,
-    [Ship_type]      NVARCHAR (50) NULL,
-    [Width]          DECIMAL (18)  NULL,
-    [Length]         DECIMAL (18)  NULL,
-    [Draught]        DECIMAL (18)  NULL,
-    [DateCreated]    DATETIME2 (7) CONSTRAINT [DF_Vessel] DEFAULT (getdate()) NULL,
-    [ValidFrom]      DATE          NULL,
-    [ValidTo]        DATE          NULL,
+    [Vessel_Key]     INT             IDENTITY (1, 1) NOT NULL,
+    [MMSI]           CHAR (9)        NULL,
+    [IMO]            CHAR (10)       NULL,
+    [Type_of_mobile] NVARCHAR (20)   NULL,
+    [Call_Sign]      NVARCHAR (20)   NULL,
+    [Name]           NVARCHAR (50)   NULL,
+    [Ship_type]      NVARCHAR (50)   NULL,
+    [Width]          DECIMAL (10, 2) NULL,
+    [Length]         DECIMAL (10, 2) NULL,
+    [Draught]        DECIMAL (10, 2) NULL,
+    [DateCreated]    DATETIME2 (7)   CONSTRAINT [DF_Vessel] DEFAULT (getdate()) NULL,
+    [ValidFrom]      DATE            NULL,
+    [ValidTo]        DATE            NULL,
     CONSTRAINT [PK_Vessel] PRIMARY KEY CLUSTERED ([Vessel_Key] ASC)
 );
+
+
 
 
 
--- AIS_Stage/load/Stored Procedures/Dim_Vessel_SP.sql
+++ AIS_Stage/load/Stored Procedures/Dim_Vessel_SP.sql
@@ -1,13 +1,8 @@
 ﻿
-
-
-
-
-
 CREATE PROCEDURE [load].[Dim_Vessel_SP]
 @countr int = 0
 AS
-set @countr = (select count(*) from AIS_EDW.edw.Dim_Vessel where MMSI = (select MMSI from stage.D_Vessel))
+set @countr = (select count(*) as cnt from AIS_EDW.edw.Dim_Vessel where MMSI in (select MMSI from AIS_Stage.transform.Dim_Vessel))
  if (@countr = 0) 
  BEGIN  
 		INSERT INTO AIS_EDW.edw.Dim_Vessel
@@ -35,5 +30,52 @@ set @countr = (select count(*) from AIS_EDW.edw.Dim_Vessel where MMSI = (select 
 			Length,
 			Draugth 
 			--DateCreated
-		 FROM transform.Dim_Vessel)
+		 FROM AIS_Stage.transform.Dim_Vessel)
 END
+ else
+	begin
+		Declare @MMSI nvarchar(50), @IMO nvarchar(50)    
+		Declare myCursor CURSOR FOR select MMSI from AIS_EDW.edw.Dim_Vessel where MMSI in (select MMSI from AIS_Stage.transform.Dim_Vessel)
+		Open myCursor    
+		--Fetch next from ProductCursor into @ProductID  
+  
+		while(@@FETCH_STATUS=0)  
+		BEGIN  
+  
+		Set @IMO = (select IMO from AIS_Stage.transform.Dim_Vessel)  
+			BEGIN  
+			update AIS_EDW.edw.Dim_Vessel   set [ValidTo]=GETDATE()  where @IMO=IMO
+				INSERT INTO AIS_EDW.edw.Dim_Vessel
+					(MMSI, 
+					IMO, 
+					Type_of_mobile, 
+					Call_Sign, 
+					Name, 
+					Ship_type, 
+					Width, 
+					Length, 
+					Draught
+					--DateCreated 	
+					--ValidFrom,
+					--ValidTo, 
+					 ) 
+				( SELECT 
+					MMSI, 
+					IMO, 
+					Type_of_mobile, 
+					Call_Sign, 
+					Name, 
+					Ship_type, 
+					Width, 
+					Length,
+					Draugth 
+					--DateCreated
+				 FROM transform.Dim_Vessel)
+		END  
+  
+		Fetch next from myCursor into @MMSI  
+		END  
+  
+		Close myCursor   
+		DEALLOCATE myCursor  
+		end

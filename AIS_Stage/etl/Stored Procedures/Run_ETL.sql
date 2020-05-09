9a6ead54e6ed911379faa00204cf4114115ba533
napav <napav@systematic.com>
2020-05-09 09:47:23 +0200

    updated SSIS handling

---
+++ AIS_Stage/etl/Stored Procedures/Run_ETL.sql
@@ -0,0 +1,47 @@
+﻿
+
+
+/*
+Change log: 
+	2020-05-08	NP	Stored procedure created
+*/
+
+CREATE PROCEDURE [etl].[Run_ETL] (@continuous smallint)
+AS
+
+BEGIN TRY
+	BEGIN TRANSACTION
+		EXEC extract.AIS_Data_CSV
+		EXEC load.Dim_Vessel_L
+		EXEC load.Dim_Voyage_L
+		EXEC load.Fact_Route_L
+
+		INSERT INTO utility.Batch (
+			Batch,
+			TotalRows,
+			IsContinuousData,
+			IsHistoricalData,
+			MinReceivedTime,
+			MaxReceivedTime,
+			DateCreated )
+		SELECT DISTINCT
+			Batch,
+			count(*),
+			@continuous,
+			ABS(@continuous - 1),
+			MIN(ReceivedTime),
+			MAX(ReceivedTime),
+			GETDATE()		
+		FROM extract.AIS_Data
+		GROUP BY Batch
+
+		EXEC archive.AIS_Data_AddToArchive
+
+		TRUNCATE table extract.AIS_Data
+	COMMIT
+END TRY
+
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRAN
+END CATCH

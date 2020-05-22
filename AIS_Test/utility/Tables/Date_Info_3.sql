107eef147fa0b3bb0aa7d302ba06633edacb625a
stefy8700isc <stefy8700isc@gmail.com>
2020-05-22 14:04:41 +0200

    test

---
+++ AIS_Test/utility/Tables/Date_Info_3.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [utility].[Date_Info] (
+    [DateKey]             INT           NOT NULL,
+    [Date]                DATE          NOT NULL,
+    [Year]                INT           NOT NULL,
+    [Quarter]             INT           NOT NULL,
+    [Month]               INT           NOT NULL,
+    [Month_Name]          NVARCHAR (10) NOT NULL,
+    [Month_ShortName]     NVARCHAR (3)  NOT NULL,
+    [Week]                INT           NOT NULL,
+    [Day]                 INT           NOT NULL,
+    [DayOfYear]           INT           NOT NULL,
+    [DayOfWeek]           INT           NOT NULL,
+    [DayOfWeek_Name]      NVARCHAR (10) NOT NULL,
+    [DayOfWeek_ShortName] NVARCHAR (3)  NOT NULL
+);
+

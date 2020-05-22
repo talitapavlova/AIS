3cc141b49a86743434a26ec0bf62a39ca28204ad
stefy8700isc <stefy8700isc@gmail.com>
2020-05-22 12:42:24 +0200

    Revert "Even_Log tests added"
    This reverts commit b16d3cf096a1fdeff74a6716c2654923405a131d.

--- AIS_Test/Security/testEventLog.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE SCHEMA [testEventLog]
-    AUTHORIZATION [dbo];
-
---
+++ AIS_Test/test/Stored Procedures/Run all the tests.sql
@@ -0,0 +1,11 @@
+﻿
+
+
+-- Run All tests
+
+CREATE       PROCEDURE [test].[Run all the tests]
+AS
+	EXEC tSQLt.Run '[testDimVessel]'
+	EXEC tSQLt.Run '[testBatch]'
+	EXEC tSQLt.Run '[testDimVoyage]'
+	EXEC tSQLt.Run '[testFactRoute]'

487f492acfb5d3e82db05e7ebfbe6d1527e4ce03
stefy8700isc <stefy8700isc@gmail.com>
2020-05-22 12:55:06 +0200

    Delete Dim_Navigation_Status_2.sql

--- AIS_Test/edw/Tables/Dim_Navigation_Status_2.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [edw].[Dim_Navigation_Status] (
-    [Navigation_Status_Key]         INT            NOT NULL,
-    [Navigation_Status_Description] NVARCHAR (500) NULL,
-    CONSTRAINT [PK_Dim_Navigation_Status] PRIMARY KEY CLUSTERED ([Navigation_Status_Key] ASC)
-);
-
